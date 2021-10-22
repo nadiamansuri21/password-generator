b919c42df0a029932c9ab951aaa4db59ad296907
nadiaalamgir21 <87517265+nadiaalamgir21@users.noreply.github.com>
2021-10-22 11:14:21 -0700

    random password generator create.

--- Develop/script.js
+++ Develop/script.js
@@ -25,20 +25,32 @@ function generatePassword() {
   var lowerCase = lowerCaseRequired();
   var upperCase = upperCaseRequired();
 
+  console.log(specialCharacters)
+
   //logics
   let mainArr = []
   if (specialCharacters)
-    mainArr.concat(['!', '@', '#', '$', '%', '&', '*'])
+    mainArr = mainArr.concat(['!', '@', '#', '$', '%', '&', '*'])
 
   if (numericCharacters)
-    mainArr.concat(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+    mainArr = mainArr.concat(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
 
   if (lowerCase)
-    mainArr.concat(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"])
+    mainArr = mainArr.concat(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"])
 
   if (upperCase)
-    mainArr.concat(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"])
+    mainArr = mainArr.concat(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"])
 
+  let finalPass = ''
+
+  for (let i = 0; i < passwordLength; i++) {
+    let randomVal = Math.random() 
+    let randomIndex = randomVal * mainArr.length
+    let finalIndex = Math.floor(randomIndex)
+    finalPass += mainArr[finalIndex]
+  }
+  
+  return finalPass
 }
 
 
